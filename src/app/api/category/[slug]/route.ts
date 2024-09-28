c673565e61aab92989a2d8df15089f35a97fff55
Mwero Abdalla <mwerothewebmaker@gmail.com>
2024-09-29 00:27:12 +0300

    chore: create route for /category

--- src/app/api/category/[slug]/route.ts
+++ src/app/api/category/[slug]/route.ts
@@ -1,34 +1,34 @@
-import { productService } from "@/contollers/ProductService";
 import { NextRequest, NextResponse } from "next/server";
 import { IQuery } from "../../types";
+import { Category } from "@/models/Category";
 
-//GET  api/product/slug
+//GET  api/category/slug
 export async function GET(req: NextRequest, { params: { slug } }: IQuery) {
   try {
-    const fetchedProduct = await productService.getById(slug);
-    if (!fetchedProduct)
+    const fetchedCategory = await Category.findById(slug);
+    if (!fetchedCategory)
       return NextResponse.json(
         { sucess: false, message: "Not Found" },
         { status: 404 }
       );
 
-    return NextResponse.json({ sucess: true, data: fetchedProduct });
+    return NextResponse.json({ sucess: true, data: fetchedCategory });
   } catch (error: any) {
     return NextResponse.json({ error: error.message }, { status: 500 });
   }
 }
-//POST /api/product/slug
+//POST /api/category/slug
 export async function PUT(req: NextRequest, { params: { slug } }: IQuery) {
   try {
     const body = await req.json();
-    const updatedProduct = await productService.update(slug, body);
-    if (!updatedProduct)
+    const updatedCategory = await Category.findByIdAndUpdate(slug, body);
+    if (!updatedCategory)
       return NextResponse.json(
-        { sucess: false, message: "Could not update product" },
+        { sucess: false, message: "Could not update category" },
         { status: 400 }
       );
 
-    return NextResponse.json({ sucess: true, data: updatedProduct });
+    return NextResponse.json({ sucess: true, data: updatedCategory });
   } catch (error: any) {
     return NextResponse.json({ error: error.message }, { status: 500 });
   }
@@ -36,14 +36,14 @@ export async function PUT(req: NextRequest, { params: { slug } }: IQuery) {
 
 export async function DELETE(req: NextRequest, { params: { slug } }: IQuery) {
   try {
-    const deletedProduct = await productService.getById(slug);
-    if (!deletedProduct)
+    const deletedCategory = await Category.findByIdAndDelete(slug);
+    if (!deletedCategory)
       return NextResponse.json(
         { sucess: false, message: "Not Found" },
         { status: 404 }
       );
 
-    return NextResponse.json({ sucess: true, data: deletedProduct });
+    return NextResponse.json({ sucess: true, data: deletedCategory });
   } catch (error: any) {
     return NextResponse.json({ error: error.message }, { status: 500 });
   }
---
+++ src/app/api/category/route.ts
@@ -0,0 +1,36 @@
+import { Category } from "@/models/Category";
+import { NextRequest, NextResponse } from "next/server";
+
+//GET  api/category
+export async function GET(req: NextRequest) {
+  try {
+    const fetchedCategorries = await Category.find({});
+    if (!fetchedCategorries)
+      return NextResponse.json(
+        { sucess: false, message: "Not Found" },
+        { status: 404 }
+      );
+
+    return NextResponse.json({ sucess: true, data: fetchedCategorries });
+  } catch (error: any) {
+    return NextResponse.json({ error: error }, { status: 500 });
+  }
+}
+//POST /api/category
+export async function POST(req: NextRequest) {
+  try {
+    const body = await req.json();
+    const newCategory = await Category.insertMany(
+      Array.isArray(body) ? body : [body]
+    );
+    if (!newCategory)
+      return NextResponse.json(
+        { sucess: false, message: "Could not create category" },
+        { status: 400 }
+      );
+
+    return NextResponse.json({ sucess: true, data: newCategory });
+  } catch (error: any) {
+    return NextResponse.json({ error: error }, { status: 500 });
+  }
+}
