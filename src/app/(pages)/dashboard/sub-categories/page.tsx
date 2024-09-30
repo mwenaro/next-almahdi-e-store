0037c10f8bc610f1b24093d346604789f8eea8ef
Mwero Abdalla <mwerothewebmaker@gmail.com>
2024-09-30 19:50:28 +0300

    fix : subs

--- src/app/(pages)/dashboard/sub-categories/page.tsx
+++ src/app/(pages)/dashboard/sub-categories/page.tsx
@@ -1,30 +1,29 @@
-export const revalidate = 0
+export const revalidate = 0;
 import { Breadcrumbs } from "@/components/breadcrumbs";
 import PageContainer from "@/components/layout/page-container";
 import { SubCategoryClient } from "@/components/tables/sub-category-tables/client";
 import axios from "axios";
 import { headers } from "next/headers";
 const getSubCategories = async () => {
-  const url =  headers().get("x-url");
-  const formatedUrl = `${url}/api/sub-category`
-  console.log({ url , formatedUrl});
+  const url = headers().get("x-url");
+  const formatedUrl = `${url}/api/sub-category`;
+  console.log({ url, formatedUrl });
   // const res = axios.get('')
   // const url = process.env.__NEXT_PRIVATE_ORIGIN;
 
-  // const res = await axios.get(`${url}/api/sub-category`)
-  const res = await axios.get(`${process.env.NEXTAUTH_URL!}/api/sub-category`)
-  console.log(res.data.data)
+  const res = await axios.get(`${url}/api/sub-category`);
+  // const res = await axios.get(`${process.env.NEXTAUTH_URL!}/api/sub-category`)
+  // console.log(res.data.data)
   // if(!res?.data?.length) return []
-  return await res.data.data || []
+  return (await res.data.data) || [];
 };
 
 const breadcrumbItems = [
   { title: "Dashboard", link: "/dashboard" },
-  { title: "Categories", link: "/dashboard/categories" },
+  { title: "SubCategories", link: "/dashboard/sub-categories" },
 ];
 export default async function page() {
   const myData = await getSubCategories();
-  
 
   return (
     <PageContainer>
