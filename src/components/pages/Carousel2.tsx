4ad5b43de9fddaffdcc7c90bb481b487cc8550b6
Mwero Abdalla <mwerothewebmaker@gmail.com>
2024-10-10 22:20:42 +0300

    fix :
     bug-fix in ui

--- src/components/pages/Carousel2.tsx
+++ src/components/pages/Carousel2.tsx
@@ -50,41 +50,41 @@ const Carousel2: React.FC<CarouselProps> = ({ images }) => {
     <section className="w-full h-fit overflow-hidden ">
       <div className="relative w-full h-[10vh] sm:h-[17vh] md:h-[20vh] lg:h-[30vh] xl:h-[60vh] overflow-hidden">
         {/* <div className="overflow-hidden h-full"> */}
-          <AnimatePresence initial={false} custom={direction}>
-            <motion.div
-              key={currentIndex}
-              className="absolute w-full h-full"
-              custom={direction}
-              variants={variants}
-              initial="enter"
-              animate="center"
-              exit="exit"
-              transition={{
-                x: { type: "spring", stiffness: 300, damping: 30 },
-                opacity: { duration: 0.2 },
-              }}
-            >
-              <Image
-                alt={`Carousel Image ${currentIndex + 1}`}
-                src={images[currentIndex]}
-                width={1920}
-                height={1080}
-                className="object-cover w-full "
-                layout="responsive"
-              />
-            </motion.div>
-          </AnimatePresence>
+        <AnimatePresence initial={false} custom={direction}>
+          <motion.div
+            key={currentIndex}
+            className="absolute w-full h-full"
+            custom={direction}
+            variants={variants}
+            initial="enter"
+            animate="center"
+            exit="exit"
+            transition={{
+              x: { type: "spring", stiffness: 300, damping: 30 },
+              opacity: { duration: 0.2 },
+            }}
+          >
+            <Image
+              alt={`Carousel Image ${currentIndex + 1}`}
+              src={images[currentIndex]}
+              width={1920}
+              height={1080}
+              className="object-cover w-full "
+              layout="responsive"
+            />
+          </motion.div>
+        </AnimatePresence>
         {/* </div> */}
         {/* Responsive button styles */}
         <button
           onClick={handlePrev}
-          className="absolute  left-4 top-1/2 -translate-y-1/2 bg-white p-2 sm:p-3 md:p-4 lg:p-5 rounded-full z-10 hidden sm:block hover:bg-gray-200 transition-all"
+          className="absolute left-4 top-1/2 -translate-y-1/2 bg-white p-2 sm:p-3 md:p-4 lg:p-5 rounded-full z-10 hidden sm:block"
         >
           Prev
         </button>
         <button
           onClick={handleNext}
-          className="absolute  right-4 top-1/2 -translate-y-1/2 bg-white p-2 sm:p-3 md:p-4 lg:p-5 rounded-full z-10 hidden sm:block hover:bg-gray-200 transition-all"
+          className="absolute right-4 top-1/2 -translate-y-1/2 bg-white p-2 sm:p-3 md:p-4 lg:p-5 rounded-full z-10 hidden sm:block"
         >
           Next
         </button>
